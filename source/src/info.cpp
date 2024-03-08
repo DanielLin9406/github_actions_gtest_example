c935f571b313cb7ba2127c9fd40155b5cff62478
Chun-Cheng Lin <clin105@mySeneca.ca>
2024-03-07 22:07:52 -0500

    update

--- source/src/info.cpp
+++ source/src/info.cpp
@@ -1,10 +1,11 @@
-#include <github_actions_gtest_example/github_actions_gtest_example.h>
+// #include <github_actions_gtest_example/github_actions_gtest_example.h>
+#include "../header/github_actions_gtest_example/testing.h"
 
 #include <iostream>
 
 int main(int argc, char** argv) {
 
-  std::cout << githubActionsGtestExample::sayHello() << std::endl;
+  // std::cout << githubActionsGtestExample::sayHello() << std::endl;
 
   return 0;
 }
